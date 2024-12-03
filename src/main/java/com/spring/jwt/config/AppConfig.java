712d7e4291e48c1ac08edb5895ad3eece114a590
Geetesh Kumbalkar <geetesh.kumbalkar@wittygreens.com>
2024-12-03 15:41:25 +0530

    ok

--- src/main/java/com/spring/jwt/config/AppConfig.java
+++ src/main/java/com/spring/jwt/config/AppConfig.java
@@ -94,19 +94,6 @@ public class AppConfig {
                 .formLogin().disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/account/**").permitAll()
-                .requestMatchers(
-                        "/api/v1/auth/**",
-                        "/v2/api-docs",
-                        "/v3/api-docs",
-                        "/v*/a*-docs/**",
-                        "/swagger-resources",
-                        "/swagger-resources/**",
-                        "/configuration/ui",
-                        "/configuration/security",
-                        "/swagger-ui/**",
-                        "/webjars/**",
-                        "/swagger-ui.html"
-                ).permitAll()
                 .requestMatchers("/cars/**").permitAll()
                 .requestMatchers("/finalBid/**").permitAll()
                 .requestMatchers("/BeadingCarController/**").permitAll()
